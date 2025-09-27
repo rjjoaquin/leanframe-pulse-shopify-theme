52c6255544badaf32692bd3108b297988f4e0e92
rjjoaquin <joaquinrubenjay@gmail.com>
2025-09-26 22:24:06 -0500

    further css corrections

--- assets/global-elements.js
+++ assets/global-elements.js
@@ -4,22 +4,5 @@ class LeanframeForm extends HTMLElement {
     this.allSelectButtons = this.querySelectorAll("select-button");
     this.allTextInput = this.querySelectorAll('input[type="text"]');
   }
-  connectedCallback() {
-    if (this.allSelectButtons.length > 0) {
-      this.allSelectButtons.forEach((selectButton) => {
-        selectButton
-          .querySelector('input[type="radio"]')
-          .addEventListener("change", (e) => {
-            const parentForm = e.target.closest("form");
-            const formName = e.target.name;
-            if (parentForm) {
-              parentForm
-                .querySelectorAll(`select-button input[name="${formName}"]`)
-                .removeAttribute("checked");
-            }
-            e.target.checked = true;
-          });
-      });
-    }
-  }
+  connectedCallback() {}
 }
