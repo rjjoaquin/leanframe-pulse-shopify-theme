4e73c4c70d90b06185b0875c84adef56eab03f13
rjjoaquin <joaquinrubenjay@gmail.com>
2025-11-17 01:43:05 +0800

    added submenu to desktop

--- assets/header.js
+++ assets/header.js
@@ -1,30 +1,58 @@
 class HeaderMenu extends HTMLElement {
   constructor() {
     super();
+    // this runs on the assumption that shopify has max three level menus
     this.menuHasSubmenu = this.querySelectorAll("[data-has-submenu]");
+    this.submenuHasGrandchild = this.querySelectorAll("[data-has-grandchild]");
     this.mobileMenu = this.querySelector(".mobile-menu");
   }
 
   connectedCallback() {
-    this.menuHasSubmenu.forEach((subMenu) => {
-      subMenu.addEventListener("mouseover", () => {
-        this.menuHasSubmenu.forEach((menu) => {
-          if (menu !== subMenu) {
-            this.closeSubmenu(menu);
-          } else {
-            if (
-              !subMenu
-                .querySelector("[data-submenu]")
-                .classList.contains("flex")
-            )
-              this.openSubmenu(subMenu);
-          }
+    if (this.menuHasSubmenu.length > 0) {
+      this.menuHasSubmenu.forEach((subMenu) => {
+        subMenu.addEventListener("mouseover", () => {
+          this.menuHasSubmenu.forEach((menu) => {
+            if (menu !== subMenu) {
+              this.closeSubmenu(menu);
+            } else {
+              if (
+                !subMenu
+                  .querySelector("[data-submenu]")
+                  .classList.contains("flex")
+              )
+                this.openSubmenu(subMenu);
+            }
+          });
+        });
+        subMenu.addEventListener("mouseout", () => {
+          this.closeSubmenu(subMenu);
         });
       });
-      subMenu.addEventListener("mouseout", () => {
-        this.closeSubmenu(subMenu);
+    }
+
+    if (this.submenuHasGrandchild.length > 0) {
+      this.submenuHasGrandchild.forEach((grandchildMenu) => {
+        grandchildMenu.addEventListener("mouseover", () => {
+          this.submenuHasGrandchild.forEach((menu) => {
+            if (menu != grandchildMenu) {
+              this.closeSubmenu(menu, true);
+            } else {
+              if (
+                !grandchildMenu
+                  .querySelector("[data-grandchildmenu]")
+                  .classList.contains("flex")
+              ) {
+                this.openSubmenu(grandchildMenu, true);
+              }
+            }
+          });
+        });
+        grandchildMenu.addEventListener("mouseout", () => {
+          this.closeSubmenu(grandchildMenu, true);
+        });
       });
-    });
+    }
+
     this.mobileMenu
       ?.querySelector(".close-button")
       .addEventListener("click", () => {
@@ -32,31 +60,26 @@ class HeaderMenu extends HTMLElement {
       });
   }
 
-  openSubmenu(subMenu) {
-    const sub = subMenu.querySelector("[data-submenu]");
+  openSubmenu(subMenu, isGrandchild = false) {
+    const sub = subMenu.querySelector(
+      `${isGrandchild === true ? "[data-grandchildmenu]" : "[data-submenu]"}`
+    );
     if (sub) {
       sub.classList.remove("hidden");
       sub.classList.add("flex");
-      if (sub.closest("[data-menu-item]")) {
-        const menuLink = sub
-          .closest("[data-menu-item]")
-          .querySelector("[data-menu-link]");
-        const dataHref = menuLink.getAttribute("data-href");
-        menuLink.setAttribute("href", dataHref);
-      }
     }
   }
-  closeSubmenu(subMenu) {
-    const sub = subMenu.querySelector("[data-submenu]");
+  closeSubmenu(subMenu, isGrandchild = false) {
+    const sub = subMenu.querySelector(
+      `${isGrandchild === true ? "[data-grandchildmenu]" : "[data-submenu]"}`
+    );
     if (sub) {
+      if (isGrandchild) {
+        console.log("closing this", sub);
+      }
+
       sub.classList.add("hidden");
       sub.classList.remove("flex");
-      if (sub.closest("[data-menu-item]")) {
-        const menuLink = sub
-          .closest("[data-menu-item]")
-          .querySelector("[data-menu-link]");
-        menuLink.removeAttribute("href");
-      }
     }
   }
 
