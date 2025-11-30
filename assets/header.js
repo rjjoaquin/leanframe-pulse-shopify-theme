c7de4611cac6310da0501e8e628f8f7e068a9b0d
rjjoaquin <joaquinrubenjay@gmail.com>
2025-11-30 09:07:20 -0600

    added fix for mouse leave

--- assets/header.js
+++ assets/header.js
@@ -4,6 +4,9 @@ class HeaderMenu extends HTMLElement {
     // this runs on the assumption that shopify has max three level menus
     this.mobileMenu = this.querySelector(".mobile-menu");
     this.closeButton = this.querySelector(".mobile-menu .close-button");
+    this.headerMenu = this.querySelector("[data-header-menu]");
+    this.topLevelItems = this.querySelectorAll("[data-top-level-item]");
+    this.megaMenuItems = this.querySelector("[data-megamenu-items]");
   }
 
   connectedCallback() {
@@ -11,10 +14,48 @@ class HeaderMenu extends HTMLElement {
       this.mobileMenu.open = false;
       this.mobileMenu.classList.remove("open");
     });
+
+    if (this.topLevelItems && this.megaMenuItems) {
+      this.topLevelItems.forEach((item) => {
+        item.addEventListener("mouseover", (e) => {
+          const tag = e.target.getAttribute("data-tag");
+          if (tag) {
+            this.openMegamenuItem(tag);
+          }
+        });
+      });
+      if (this.headerMenu) {
+        this.addEventListener("mouseleave", () => {
+          this.closeMegamenuItems();
+        });
+      }
+    }
   }
 
   toggleMobileMenu() {
     this.mobileMenu.open = !this.mobileMenu.open;
+  }
+
+  openMegamenuItem(tag) {
+    if (this.megaMenuItems) {
+      this.megaMenuItems.classList.remove("z-[-999]", "h-0", "w-0");
+      this.megaMenuItems.classList.add("z-2", "h-auto", "w-full");
+      this.megaMenuItems
+        .querySelector(`[item-tag="${tag}"`)
+        .classList.remove("hidden");
+    }
+  }
+
+  closeMegamenuItems() {
+    if (this.megaMenuItems) {
+      this.megaMenuItems.classList.add("z-[-999]", "h-0", "w-0");
+      this.megaMenuItems.classList.remove("z-2", "h-auto", "w-full");
+      this.megaMenuItems
+        .querySelectorAll("[data-megamenu-item]")
+        .forEach((item) => {
+          item.classList.add("hidden");
+        });
+    }
   }
 }
 
